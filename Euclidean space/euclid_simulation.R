d694d845430471615f6a8bb54180d1ac20b528e6
YangdiJiang <65643288+YangdiJiang@users.noreply.github.com>
2023-11-08 23:04:51 -0700

    Create euclid_simulation.R

---
+++ Euclidean space/euclid_simulation.R
@@ -0,0 +1,40 @@
+rm(list =ls())
+path <- "/Users/yangdi/Library/Mobile Documents/com~apple~CloudDocs/My Documents/Graduate School Materials/Graduate Projects/GDP on Manifold/Euclidean/"
+# path <- "C:\\Users\\jiang\\iCloudDrive\\My Documents\\Graduate School Materials\\Graduate Projects\\GDP on Manifold\\Euclidean\\"
+source(paste0(path, "euclid_functions.R"))
+
+n_trial <- 20
+
+sensi <- 1
+n_eps <- 1000
+n <- 1000
+m <- 100
+d = 1
+
+for (z in seq(0.25, 4, 0.25)){
+  sigma <- z * sensi
+  eps_max <- max(5 * sensi / sigma + sensi^2 / (2 * sigma^2), 10)
+  # parallelization 
+  no_cores <- parallel::detectCores() - 2
+  cl <- parallel::makeCluster(no_cores)
+  parallel::clusterExport(cl, c("path", "sensi", "sigma", "eps_max", "n_eps", "n", "m", "n_trial", "z", "d"))
+  parallel::clusterEvalQ(cl, source(paste0(path, "euclid_functions.R")))
+  
+  
+  print(Sys.time())
+  # gdp_u_alt(sensi = sensi, sigma = sigma, d = 2, n = n, eps_max = eps_max, n_eps = n_eps, type = "gauss", m = 2)$u
+  # print(Sys.time())
+  
+  res_list <- parallel::parSapply(cl, 1:n_trial, function(i){
+    # dist_list <- sapply(1:100, function(i){  
+    gdp_u_alt(sensi = sensi, sigma = sigma, d = d, n = n, eps_max = eps_max, n_eps = n_eps, type = "gauss", m = m)$u
+  })
+  
+  k <- 1
+  
+  u_true <- sensi / sigma
+  res_list <- cbind(rep(u_true, n_trial), res_list)
+  write.csv(res_list, file = paste0(path, "euclid_result_", k, "_", d, "_", sensi, "_", z, ".csv"))
+  parallel::stopCluster(cl)
+  print(Sys.time())
+}
